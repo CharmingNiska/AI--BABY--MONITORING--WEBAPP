eb36d65dc1b266ad25bd4d812589cb1d5c3bfe62
CharmingNiska <71717872+CharmingNiska@users.noreply.github.com>
2020-10-23 19:46:22 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,64 +1,34 @@
-img = "";
-status = "";
-object = [];
+
+video = "";
 
 function preload()
 {
- img = loadImage('BTS-World.jpg');
+  vidoe = createVideo('video.mp4');
+  video.hide();
 }
 
 function setup()
 {
-    canvas = createCanvas(380, 380);
+    canvas = createCanvas(480, 400);
     canvas.center();
-    video = createCapture(VIDEO);
-    video.size(380,380);
-    video.hide();
+}
+
+function draw()
+{
+    image(video, 0, 0, 480, 400);
+}
+
+function start()
+{
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML = "Status : Detecting Objects";
-
 }
 
 function modelLoaded()
 {
-    console.log("Model loaded!")
-    status = true;
-    
-}
-
-
-function gotResult(error, results) 
-{
-    if (error) {
-        console.log(error);
-    }
-    console.log(results);
-    objects = results;
-}
- 
-function draw()
-{
-    image(video, 0, 0, 380, 380);
- if(status != "")
- {
-     r = random(255);
-     g = random(255);
-     b = random(255);
-     objectDetector.detect(video, gotResult);
-    for (i = 0; i < objects.length; i++){
-         document.getElementById("status").innerHTML = "Status : Object Detected";
-         document.getElementById("number-of-object").innerHTML = " : Number of Object Detected is : "+object.length;
-
-         fill(r,g,b);
-         percent = floor(objects[i].confidence * 100);
-         text(objects[i].label + "" + percent + "%", objects[i].x + 15, objects[i].y + 15);
-         notFill();
-         stroke(r,g,b);
-         rect(object[i].x, object[i].y, object[i].width, object[i].height);
-    } 
-  }
-
-    
-   
-
+console.log("Model Loaded!");
+status = true;
+video.loop();
+video.speed(1);
+video.volume(0);
 }
